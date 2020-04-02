c52f00ba87dcf1ee65a66b0a2bfcc9e60183d6ef
Valentin <someadress3000@gmail.com>
2020-04-02 14:17:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.hello_world
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mHelloTextView: TextView;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+         imageButton.setOnClickListener(){
+             if(editText.text.isEmpty()) {
+                 textView.text = " Hello Kitty!"
+             } else {
+              textView.text = "Привет, " + editText.text
+             }
+         }
+    }
+}
